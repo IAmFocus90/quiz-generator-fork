cb81ac588678542bbc49a41b4db65230c5ba4126
Emmanuel Enwelunta <emmanuelenwelunta@gmail.com>
2025-07-25 07:37:50 +0100

    update celery_config with use_ssl settings

--- server/celery_config.py
+++ server/celery_config.py
@@ -1,10 +1,17 @@
 from celery import Celery
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
+
 
 load_dotenv()
 
 redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+parsed = urlparse(redis_url)
+
+# Check if using secure Redis (rediss://)
+use_ssl = parsed.scheme == "rediss"
+
 
 
 celery_app = Celery(
@@ -12,6 +19,15 @@ celery_app = Celery(
     broker=redis_url,
     backend=redis_url,
 )
+
+if use_ssl:
+    celery_app.conf.broker_use_ssl = {
+        "ssl_cert_reqs": "CERT_NONE"
+    }
+    celery_app.conf.redis_backend_use_ssl = {
+        "ssl_cert_reqs": "CERT_NONE"
+    }
+
 
 celery_app.conf.task_routes = {
     "server.tasks.send_quiz_email": {"queue": "email"}
