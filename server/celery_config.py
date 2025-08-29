846b4a073131e4354450076992b1934cff225f8b
Emmanuel Enwelunta <emmanuelenwelunta@gmail.com>
2025-08-29 07:45:22 +0100

    clean up redundant codes

--- server/celery_config.py
+++ server/celery_config.py
@@ -26,37 +26,3 @@ celery_app.conf.task_routes = {
 
 # Ensure tasks are registered
 import server.app.share.share_tasks  # noqa: E402,F401
-
-
-
-
-
-
-
-
-
-# import os
-# from urllib.parse import urlparse
-# import ssl
-# from celery import Celery
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# redis_url = os.getenv("REDIS_URL")
-# celery_app = Celery("quiz_app", broker=redis_url, backend=redis_url)
-
-# parsed = urlparse(redis_url)
-# if parsed.scheme == "rediss":
-#     celery_app.conf.broker_use_ssl = {
-#         "ssl_cert_reqs": ssl.CERT_NONE
-#     }
-#     celery_app.conf.redis_backend_use_ssl = {
-#         "ssl_cert_reqs": ssl.CERT_NONE
-#     }
-
-# celery_app.conf.task_routes = {
-#     "server.tasks.send_quiz_email": {"queue": "email"}
-# }
-
-# import server.app.share.share_tasks
